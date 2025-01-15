8e4ed9c94b3f4b00ae1a7403de66fd293643b1fb
zaiden0 <z8n.me@proton.me>
2025-01-15 15:12:28 -0500

    Removed Sleeping

--- src/main/java/net/zaiden/nosleepmod/Nosleepmod.java
+++ src/main/java/net/zaiden/nosleepmod/Nosleepmod.java
@@ -1,6 +1,8 @@
 package net.zaiden.nosleepmod;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.entity.event.v1.EntitySleepEvents;
+import net.minecraft.entity.player.PlayerEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,6 +12,14 @@ public class Nosleepmod implements ModInitializer {
 
     @Override
     public void onInitialize() {
+        EntitySleepEvents.ALLOW_SLEEPING.register((player, sleepingPos) -> {
+            return PlayerEntity.SleepFailureReason.OTHER_PROBLEM;
+        });
+
+
+
+
+
         LOGGER.info("Z <3 A = )");
     }
 }
